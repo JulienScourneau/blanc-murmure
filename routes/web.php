9bb2584cfc093557283e9280e5ff4f874f7d7a2d
Julien Scourneau <scourneaujulien@outlook.com>
2022-11-23 15:46:47 +0100

    Update route name

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\AttendeesController;
+use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\IllustrationController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\SculptureController;
 use App\Http\Controllers\StageController;
+use App\Http\Controllers\UrbanSpaceController;
+use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,11 +31,12 @@ Route::get('/construire-un-projet', function () {
 })->name('collaborater');
 
 Route::get('/nos-projets', [ProjectController::class, 'index'])->name('project');
-Route::get('/nos-projets/sculpture',[])->name('sculpture');
-Route::get('/nos-projets/illustration',)->name('illustration');
-Route::get('/nos-projets/espace-urbain',)->name('espace-urbain');
-Route::get('/nos-projets/video',)->name('video');
-Route::get('/nos-projets/catalogue',)->name('catalogue');
+
+Route::get('/nos-projets/sculpture', [SculptureController::class, 'index'])->name('sculpture');
+Route::get('/nos-projets/illustration', [IllustrationController::class, 'index'])->name('illustration');
+Route::get('/nos-projets/espace-urbain', [UrbanSpaceController::class, 'index'])->name('urbanSpace');
+Route::get('/nos-projets/video', [VideoController::class, 'index'])->name('video');
+Route::get('/nos-projets/catalogue', [CatalogController::class, 'index'])->name('catalog');
 
 Route::get('/à-propos', function () {
     return view('about');
