9a58639780810dfc63584e6de2f7d651f526ca8d
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-11-27 23:37:30 +0100

    create hover for home

--- resources/views/components/agenda.blade.php
+++ resources/views/components/agenda.blade.php
@@ -1,28 +1,30 @@
 <!-- <hr class="mt-12 text-separator"/> -->
-<h2 class="Title font-black text-3xl py-2.5 text-orange mt-10">Agenda</h2>
-<hr class="mb-6 w-28 shadow text-separator"/>
+<div class="w-6/12 ml-14 mb-5 flex flex-col items-center">
+    <h2 class="Title font-black text-3xl py-2.5 text-orange mt-10">Agenda</h2>
+    <hr class="w-28 shadow-lg border-secondaryText"/>
+</div>
 @foreach($agendas as $event)
 
-    <div class="card shadow w-11/12 flex">
-        <div class="bg-white border-orange border-2 text-center xl:w-24 flex justify-center">
-            <p class="text-3xl font-light text-orange py-5 px-1.5 w-20">{{date('d', strtotime($event['begin_at']))}}
+    <div class="card shadow w-6/12 flex max-h-28 group">
+        <div class="bg-white border-orange border-2 text-center group-hover:bg-orange xl:w-24 flex justify-center">
+            <p class="text-3xl font-light text-orange py-5 px-1.5 w-32 group-hover:text-white">{{date('d', strtotime($event['begin_at']))}}
                 <span
-                    class="uppercase text-orange font-black">
+                    class="uppercase text-orange font-black group-hover:text-white">
                     {{Carbon\Carbon::parse($event['begin_at'])->translatedFormat('M')}}
 {{--                    {{date('M', strtotime($event['begin_at']))}}--}}
                 </span>
             </p>
         </div>
-        <div class="pt-2.5 pl-2.5 bg-green xl:w-6/12">
-            <h2 class="Title uppercase font-black text-2xl leading-7 text-white">{{$event['title']}}</h2>
-            <p class="text-sm font-normal leading-tight py-0.5 text-white">Projet de fresque avec La HELHA à La
+        <div class="pt-2.5 pl-2.5 bg-green xl:w-5/12 group-hover:bg-white">
+            <h2 class="Title uppercase font-black text-2xl leading-7 text-white group-hover:text-green">{{$event['title']}}</h2>
+            <p class="text-sm font-normal leading-tight py-0.5 text-white group-hover:text-green">Projet de fresque avec La HELHA à La
                 Louvière.</p>
         </div>
-        <div class="hidden xl:block w-6/12 bg-orange">
-            <img src="" alt="">
+        <div class="hidden xl:block w-6/12 overflow-hidden">
+            <img class="w-full" src="assets/agenda_test.png" alt="">
         </div>
         <div class="hidden xl:block w-24">
-            <p class="text-6xl font-black flex justify-center h-full h-full items-center">></p>
+            <p class="text-6xl font-black flex justify-center h-full h-full items-center group-hover:text-orange group-hover:translate-x-2">></p>
         </div>
     </div>
 @endforeach
--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -3,13 +3,13 @@
     <img src="assets/Fresque.png" alt="">
     <div class="">
         <h1 class="Title uppercase text-center font-black text-2xl mt-6 mb-4">
-            Envie de faire <span class="text-orange underline cursor-pointer">
+            Envie de faire <span class="text-orange underline cursor-pointer highlightOrange">
                 <a href="{{ Route('news') }}">un stage</a>
             </span>
             Créatif ? Voici le prochain !
         </h1>
         <h2 class="Title uppercase font-black text-xl leading-7">{{$internship->title}}</h2>
         <h5 class="text-orange text-xs mb-6">En collaboration avec Le Collectif Innombrable</h5>
-        <a class="bg-orange rounded-3xl p-2.5 text-white text-xl" href="{{ Route ('news') }}">En savoir plus ></a>
+        <a class="bg-orange rounded-3xl p-2.5 text-white text-xl hover:bg-white hover:text-orange hover:border-orange hover:border-2" href="{{ Route ('news') }}">En savoir plus ></a>
     </div>
 </section>
