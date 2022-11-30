3c8843ca842ce7bab0fec783f526ab7c8c387368
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-11-30 11:47:11 +0100

    design card urbanSpace in project page and link in urban space to show

--- resources/views/components/ubranspace-card.blade.php
+++ resources/views/components/ubranspace-card.blade.php
@@ -1,9 +1,12 @@
 @props(['project'])
 <div>
-    <div class="mx-auto my-6 rounded-l border border-greenDuck ">
-        <a href="/nos-projets/espace-urbain/{{$project->slug}}">
-            <h2>{{$project->title}}</h2>
-
+    <div class="mx-auto my-6 rounded-xl h-32 w-8/12 hover:shadow-lg">
+        <a 
+            class="h-32 flex items-center bg-right bg-no-repeat bg-cover rounded-xl" 
+            style="background-image: url('{{asset('assets/agenda_test.png')}}');" 
+            href="/nos-projets/espace-urbain/{{$project->slug}}"
+        >
+            <h2 class="pl-10 Title font-black text-green text-2xl">{{$project->title}}</h2>
         </a>
     </div>
 </div>
