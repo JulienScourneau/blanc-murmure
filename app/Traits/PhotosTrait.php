e91c7003793356ba6a90fc3160d55fb212056dde
Julien Scourneau <scourneaujulien@outlook.com>
2022-12-19 16:49:00 +0100

    Added Photo trait

---
+++ app/Traits/PhotosTrait.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Traits;
+
+trait PhotosTrait
+{
+    public static function bootPhotosTrait()
+    {
+        static::updated(function ($model) {
+           dd('Trait', $model->getOriginal());
+        });
+    }
+}
