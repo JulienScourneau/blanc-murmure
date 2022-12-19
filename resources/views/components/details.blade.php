3989fd6f718cc86ac4f9d07b6e067ef3f5d8298b
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-12-19 15:20:28 +0100

    create video details

---
+++ resources/views/components/details.blade.php
@@ -0,0 +1,31 @@
+<div class="p-5 bg-greenDuck lg:my-5">
+    <h2 class="Title font-black text-2xl text-white flex justify-end">Non, mais tu l’as vu !</h2>
+</div>
+    <div>
+        <a class="flex justify-center items-center" target="_blank" rel="noopener noreferrer" href="">
+            <img class="w-full h-64 rounded-t-2xl" src="" height="100" width="100" alt="">
+            <img class="absolute w-24 opacity-25 rounded-full border-2 shadow-lg group-hover:opacity-100" src="{{asset('assets/icons/play.svg')}}" alt="">
+        </a>
+    </div>
+    <article class="p-2.5 lg:flex lg:justify-between">
+        <div>
+            <p class="lg:border-l-2 lg:border-green lg:my-2.5 lg:p-2.5">
+                <span class="Title text-green">Réalisé par : </span>
+                Muriel Loth et les enfants du Conseil Communal Consultatif de Colfontaine.
+            </p>
+            <p class="lg:border-l-2 lg:border-green lg:my-2.5 lg:p-2.5">
+                <span class="Title text-green">Durée : </span>
+                5’11’’
+            </p>
+            <p class="lg:border-l-2 lg:border-green lg:my-2.5 lg:p-2.5">
+                <span class="Title text-green">Production : </span>
+                2022
+            </p>
+        </div>
+        <div>
+            <p class="lg:border-l-2 lg:border-green lg:my-2.5 lg:p-2.5">
+                <span class="Title text-green">Projet organisé par : </span>
+                le Collège Communal de Colfontaine avec le soutien du service Hainaut Culture Tourisme.
+            </p>
+        </div>
+        </article>
