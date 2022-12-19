38ce0bdaae962d1d2ce414ac1dbc37468686614e
Julien Scourneau <scourneaujulien@outlook.com>
2022-12-19 16:50:15 +0100

    Updated Illustration and Catalog model

--- app/Models/Illustration.php
+++ app/Models/Illustration.php
@@ -2,18 +2,28 @@
 
 namespace App\Models;
 
+use App\Traits\PhotosTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Illustration extends Model
 {
-    use HasFactory;
+    use HasFactory, PhotosTrait;
 
     protected $guarded = [];
+
+    protected $casts = [
+        'photo_list' => 'array',
+    ];
 
     public function photos()
     {
         return $this->belongsToMany(Photo::class, 'photo_illustrations', 'illustrations_id', 'photos_id');
     }
+
+//    protected $dispatchesEvents = [
+//        'updated' => IllustrationUpdated::class,
+//        'deleted' => IllustrationDeleted::class,
+//    ];
 
 }
