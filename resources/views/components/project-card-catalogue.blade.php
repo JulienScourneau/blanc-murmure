9de49cfaa5c01b31449a2c6ee29a026024720f65
Julien Scourneau <scourneaujulien@outlook.com>
2022-12-20 11:29:43 +0100

    Bind catalogs link and thumbnail

--- resources/views/components/project-card-catalogue.blade.php
+++ resources/views/components/project-card-catalogue.blade.php
@@ -1,6 +1,6 @@
 @props(['project'])
-<div class="w-full mx-auto shadow-lg bg-greenDuck mb-5 rounded-2xl lg:m-5"> 
-    <img class="rounded-t-2xl" src="{{$project->thumbnail}}" alt="">
+<div class="w-full mx-auto shadow-lg bg-greenDuck mb-5 rounded-2xl lg:m-5">
+    <img class="rounded-t-2xl" src="{{asset('storage/'.$project->thumbnail)}}" alt="">
     <div class="text-white p-2.5">
         <h2 class="Title font-black size-2xl mb-2.5">{{$project->name}}</h2>
         <p class="border-l-2 border-white pl-2.5 lg:h-20">{{$project->description}}</p>
@@ -9,11 +9,11 @@
             <p class="border-l-2 border-white pl-2.5 ml-10">{{$project->size}}</p>
         </div>
         <div class="flex justify-around">
-            <a class="px-2.5 py-1 bg-orange rounded-full border-2 border-orange hover:bg-white hover:text-orange" href="">Aperçu ></a>
-            <a 
-                class="px-2.5 py-1 ml-1 bg-orange rounded-full border-2 border-orange hover:bg-white hover:text-orange" 
-                href="https://www.publier-un-livre.com/fr/le-livre-en-papier/2125-vertige"
-                target="_blank" rel="noopener noreferrer"
+            <a class="px-2.5 py-1 bg-orange rounded-full border-2 border-orange hover:bg-white hover:text-orange"
+               href="https://www.publier-un-livre.com/fr/le-livre-en-papier/2125-vertige">Aperçu ></a>
+            <a class="px-2.5 py-1 ml-1 bg-orange rounded-full border-2 border-orange hover:bg-white hover:text-orange"
+               href="{{$project->link}}"
+               target="_blank" rel="noopener noreferrer"
             >Commandez ></a>
         </div>
     </div>
