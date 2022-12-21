91b457aa70f86fb5dd2db4cc49bdca2673d7ae29
Julien Scourneau <scourneaujulien@outlook.com>
2022-12-21 11:09:22 +0100

    Update database migration

--- database/migrations/2022_11_18_144038_create_catalogs_table.php
+++ database/migrations/2022_11_18_144038_create_catalogs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('catalogs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->text('description');
             $table->string('link');
             $table->string('page_number');
--- database/migrations/2022_11_22_154909_create_urban_spaces_table.php
+++ database/migrations/2022_11_22_154909_create_urban_spaces_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -18,12 +17,13 @@ return new class extends Migration
             $table->string('title');
             $table->string('location');
             $table->text('description');
-            $table->string('participant');
-            $table->string('author');
-            $table->string('partnership');
+            $table->string('participant')->nullable();
+            $table->string('author')->nullable();
+            $table->string('partnership')->nullable();
             $table->foreignId('urban_space_project_id');
             $table->string('thumbnail_landscape');
             $table->string('thumbnail_portrait');
+            $table->text('photo_list')->nullable();
             $table->timestamps();
         });
     }
