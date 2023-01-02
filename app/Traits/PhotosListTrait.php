5431c0ee0563b782b5d11bd74965644c420ea3cf
Julien Scourneau <scourneaujulien@outlook.com>
2023-01-02 14:22:03 +0100

    Added conditions to delete correct photo in PhotosListTrait updated

--- app/Traits/PhotosListTrait.php
+++ app/Traits/PhotosListTrait.php
@@ -2,12 +2,26 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\Storage;
+
 trait PhotosListTrait
 {
-    public static function bootPhotosTrait()
+    public static function bootPhotosListTrait()
     {
         static::updated(function ($model) {
+           if (isset($model->getChanges()['thumbnail'])) {
+           Storage::disk('public')->delete($model->getOriginal()['thumbnail']);
+        }
+            $diff = array_diff($model->getOriginal()["photo_list"], json_decode($model->getChanges()["photo_list"]));
+            foreach ($diff as $photo) {
+                Storage::disk('public')->delete($photo);
+//                dd($photo);
+            }
 //           dd('Trait', $model->getOriginal());
+        });
+
+        static::deleted(function ($model) {
+            dd('deleted', $model->getOriginal());
         });
     }
 }
