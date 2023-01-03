1a6858f48453a887b2d7bdfb56e0dc1de3f1cc85
Julien Scourneau <scourneaujulien@outlook.com>
2023-01-03 16:22:24 +0100

    Remove old carousel

--- resources/views/projects/sculpture.blade.php
+++ resources/views/projects/sculpture.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<x-banner-project 
-    src="{{asset('assets/project/project_sculpture_banner.png')}}" 
+<x-banner-project
+    src="{{asset('assets/project/project_sculpture_banner.png')}}"
     text="La Sculpture,
     c'est d’explorer le subtil et le monumental, le volume en tant que tel et l’espace qu’il occupe.
     Des réflexions qui nous poussent à développer un regard plus aiguisé sur notre environnement
-    comme sur nous-mêmes." 
+    comme sur nous-mêmes."
 />
 
 <div class="flex flex-col mx-auto lg:flex-row flex-wrap justify-center">
@@ -17,6 +17,5 @@
 @else
 <p>Nothing to show</p>
 @endif
-<x-modal />
 </div>
 @endsection
