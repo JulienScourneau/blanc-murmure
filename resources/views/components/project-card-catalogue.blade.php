a4f81597a621dd91084cee0cf627f5f1107aba87
Moustito <moussiaux.titouan@gmail.com>
2023-01-20 10:08:07 +0100

    add animation scale to all card

--- resources/views/components/project-card-catalogue.blade.php
+++ resources/views/components/project-card-catalogue.blade.php
@@ -8,9 +8,9 @@
             <p class="border-l-2 border-white pl-2.5">{{$project->page_number}} pages</p>
             <p class="border-l-2 border-white pl-2.5 ml-10">{{$project->size}}</p>
         </div>
-        <div class="flex justify-around">
-            <a class="px-2.5 py-1 bg-orange rounded-full border-2 border-orange hover:bg-white hover:text-orange"
-               href="https://www.publier-un-livre.com/fr/le-livre-en-papier/2125-vertige">Aperçu ></a>
+        <div class="flex justify-end pr-5 pb-2.5">
+            {{-- <a class="px-2.5 py-1 bg-orange rounded-full border-2 border-orange hover:bg-white hover:text-orange"
+               href="https://www.publier-un-livre.com/fr/le-livre-en-papier/2125-vertige">Aperçu ></a> --}}
             <a class="px-2.5 py-1 ml-1 bg-orange rounded-full border-2 border-orange hover:bg-white hover:text-orange"
                href="{{$project->link}}"
                target="_blank" rel="noopener noreferrer"
