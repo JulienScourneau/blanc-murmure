13ab68987021dc586c39922f1c3cf642473a21f7
Julien Scourneau <scourneaujulien@outlook.com>
2023-01-25 09:58:20 +0100

    Updated database settings

--- config/database.php
+++ config/database.php
@@ -58,8 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => extension_loaded('pdo_mysql') && env('APP_ENV') !== 'testing' && env('APP_ENV') !== 'local' ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => resource_path('certificates/certificate.crt'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
             ]) : [],
         ],
 
