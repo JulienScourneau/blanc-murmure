2e37661631d594131db4517a660a0408dcf8bce6
Julien Scourneau <scourneaujulien@outlook.com>
2023-01-29 17:43:41 +0100

    Added pagination for project

--- app/Http/Controllers/IllustrationController.php
+++ app/Http/Controllers/IllustrationController.php
@@ -10,7 +10,7 @@ class IllustrationController extends Controller
     public function index()
     {
         return view('projects.illustration', [
-            'illustrations' => Illustration::latest()->get()
+            'illustrations' => Illustration::latest()->paginate(5)
         ]);
     }
 }
