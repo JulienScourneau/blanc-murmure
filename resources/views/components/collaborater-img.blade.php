4085719f478869ece2d5064b5dc063c014563013
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-04-23 00:34:28 +0200

    add text and image for collaborater

--- resources/views/components/collaborater-img.blade.php
+++ resources/views/components/collaborater-img.blade.php
@@ -1,8 +1,8 @@
 @props(['src', 'hover', 'text'])
-<div class="collaborater__box relative justify-center">
-    <img class="w-full lg:p-2.5 object-cover" src={{$src}} alt="">
+<div class="collaborater__box relative justify-center h-80">
+    <img class="h-full lg:p-2.5 object-cover" src={{ $src }} alt="">
     <div class="absolute top-1 bg-greenDuck w-full h-full p-2.5 lg:overflow-hidden" style="display: none">
-        <img src={{$hover}} alt="">
-        <p class="text-xl lg:text-xs">{{$text}}</p>
+        <img src={{ $hover }} alt="">
+        <p class="text-xl lg:text-xs">{{ $text }}</p>
     </div>
 </div>
