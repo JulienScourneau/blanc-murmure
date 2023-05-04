fbe3a7724c045e7e83532ff8cdb47dca5315046b
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-05-04 11:10:35 +0200

    remove animation 'espace-urbain'

--- resources/views/components/collaborater-img.blade.php
+++ resources/views/components/collaborater-img.blade.php
@@ -1,8 +1,8 @@
 @props(['src', 'hover', 'text'])
-<div class="collaborater__box relative justify-center h-96 m-5 overflow-hidden">
+<div class="collaborater__box relative justify-center h-96 mx-auto lg:mx-5 my-5 w-1/2 overflow-hidden">
     <img class="h-full object-cover" src={{ $src }} alt="">
     <div class="absolute top-0 bg-greenDuck w-full h-full overflow-hidden" style="display: none">
-        <img src={{ $hover }} alt="">
+        <img class="object-cover" src={{ $hover }} alt="">
         <p class="text-base px-2.5 lg:text-base">{{ $text }}</p>
     </div>
 </div>
--- resources/views/components/urbanspace-card.blade.php
+++ resources/views/components/urbanspace-card.blade.php
@@ -1,14 +1,13 @@
 @props(['project'])
 
 <div class="">
-    <div class="mx-auto my-6 h-32 w-full lg:w-8/12 group hover:shadow-lg">
-        <a
-            class="gradient h-32 flex bg-right bg-no-repeat bg-cover"
-            style="background-image: url('{{asset('storage/'. $project->thumbnail)}}');"
-            href="/nos-projets/espace-urbain/{{$project->slug}}"
-        >
+    <div class="mx-auto my-6 h-32 w-full lg:w-8/12 hover:shadow-lg">
+        <a class="gradient h-32 flex bg-right bg-no-repeat bg-cover"
+            style="background-image: url('{{ asset('storage/' . $project->thumbnail) }}');"
+            href="/nos-projets/espace-urbain/{{ $project->slug }}">
             <div class="h-full w-full flex items-center">
-                <h2 class="pl-10 Title uppercase font-black text-white text-2xl relative z-50 opacity-0 group-hover:opacity-100 transition-all group-hover:duration-700">{{$project->title}}</h2>
+                <h2 class="pl-10 Title uppercase font-black text-white text-2xl relative z-50">
+                    {{ $project->title }}</h2>
             </div>
         </a>
     </div>
