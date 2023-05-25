6edfa338cdb5b6d090d43b99de2ba9a96a474005
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2023-05-25 21:20:32 +0200

    rewiter banner no internship

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -5,32 +5,31 @@
       <div class="swiper-slide">
         <img class="object-fill xl:w-9/12" src="{{asset('storage/'.$internship->thumbnail)}}" alt="Bannière de Stage">
       </div>
-        @endforeach
+      @endforeach
     </div>
     <!-- <div class="swiper-button-next"></div> -->
     <!-- <div class="swiper-button-prev"></div> -->
     <!-- <div class="swiper-pagination"></div> -->
   </div>
 
-  @if ($internships = "") 
+  @if ($internships = "")
   <div class="flex justify-center">
-        <h1 class="Title uppercase text-center font-black text-2xl p-2.5 my-6">
-            Envie de faire <span class="text-orange underline cursor-pointer highlightOrange font-black Title">
-                <a class="Title font-black" href="{{ Route('news') }}">un stage</a>
-            </span>
-            Créatif ? Voici le prochain !
-        </h1>
-        <a class="bg-orange rounded-3xl ml-12 self-center p-2.5 text-white text-xl border-orange hover:bg-white hover:text-orange hover:border-2"
-           href="/en-ce-moment/#stage-et-ateliers">En savoir plus ></a>
-    </div>
+    <h1 class="Title uppercase text-center font-black text-2xl p-2.5 my-6">
+      Envie de faire <span class="text-orange underline cursor-pointer highlightOrange font-black Title">
+        <a class="Title font-black" href="{{ Route('news') }}">un stage</a>
+      </span>
+      Créatif ? Voici le prochain !
+    </h1>
+    <a class="bg-orange rounded-3xl ml-12 self-center p-2.5 text-white text-xl border-orange hover:bg-white hover:text-orange hover:border-2" href="/en-ce-moment/#stage-et-ateliers">En savoir plus ></a>
+  </div>
   @else
-    <div class="flex justify-center">
-        <h1 class="Title uppercase text-center font-black text-2xl p-2.5 my-6">
-           Il n'y a aucun stage en ce moment, allez faire un tour dans 
-           <span class="text-orange underline cursor-pointer highlightOrange font-black Title">
-                <a class="Title font-black" href="{{ Route('project') }}">nos projets</a>
-            </span> !
-        </h1>
-    </div>
+  <div class="flex justify-center">
+    <h1 class="Title uppercase text-center font-black text-2xl p-2.5 my-6">
+      Il n'y a aucun stage en ce moment, aller faire un tour dans
+      <span class="text-orange underline cursor-pointer highlightOrange font-black Title">
+        <a class="Title font-black" href="{{ Route('project') }}">nos projets</a>
+      </span> !
+    </h1>
+  </div>
   @endif
 </section>
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,10 +1,10 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
 
-<img src="{{asset('assets/blanc-murmure-logo.png')}}" class="logo" alt="Laravel Logo">
+            <img src="{{asset('assets/blanc-murmure-logo.png')}}" class="logo" alt="blanc-murmure-logo" style="width:100%;">
 
-</a>
-</td>
+        </a>
+    </td>
 </tr>
