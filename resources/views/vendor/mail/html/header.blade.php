757c0a06f686106c516282a6e964c7f6c004f906
Julien Scourneau <scourneaujulien@outlook.com>
2023-05-25 20:33:41 +0200

    publish mail resource and update logo

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+
+<img src="{{asset('assets/blanc-murmure-logo.png')}}" class="logo" alt="Laravel Logo">
+
+</a>
+</td>
+</tr>
