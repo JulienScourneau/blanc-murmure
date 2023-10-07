6262ebb3e6b8137841d9075f6cb0db205a253fd7
Julien Scourneau <scourneaujulien@outlook.com>
2023-10-07 13:37:29 +0200

    Update newsletterController for working with new mail implementation

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -4,37 +4,16 @@ namespace App\Http\Controllers;
 
 use Exception;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Mail;
 use SendinBlue\Client\Api\ContactsApi;
 use SendinBlue\Client\Configuration;
 use SendinBlue\Client\Model\CreateContact;
 
 class NewsletterController extends Controller
 {
-    public function store()
+    public function submitNewsletter()
     {
-        $attributes = request()->validate(['email' => 'required']);
-
-        $credentials = Configuration::getDefaultConfiguration()->setApiKey('api-key', config('newsletter.api_key'));
-
-        $apiInstance = new ContactsApi(
-            new Client(),
-            $credentials
-        );
-        $createContact = new CreateContact([
-            'email' => $attributes['email'],
-        ]);
-
-
-        try {
-            $apiInstance->createContact($createContact);
-
-        } catch (Exception $e) {
-            \Illuminate\Validation\ValidationException::withMessages([
-                'email' => "Une erreur est survenu lors de l'ajout à la newsletter"
-            ]);
-        }
-        return redirect('/')
-            ->with('success', 'inscription réussi');
+        Mail::to(env('MAIL_USERNAME'))->send(new NewsletterController());
     }
 
 }
